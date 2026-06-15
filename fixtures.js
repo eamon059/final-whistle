e6784419849f86916bbd13370b63b1c56476b8d5
eamon059 <eamonnaughton05@gmail.com>
2026-06-15 02:27:04 +0100

    Update fixtures.js

--- fixtures.js
+++ fixtures.js
@@ -1,1 +1,14 @@
-
+const GROUPS = [
+  { id: "A", matches: 8 },
+  { id: "B", matches: 8 },
+  { id: "C", matches: 8 },
+  { id: "D", matches: 8 },
+  { id: "E", matches: 8 },
+  { id: "F", matches: 8 },
+  { id: "G", matches: 8 },
+  { id: "H", matches: 8 },
+  { id: "I", matches: 8 },
+  { id: "J", matches: 8 },
+  { id: "K", matches: 8 },
+  { id: "L", matches: 8 }
+];
